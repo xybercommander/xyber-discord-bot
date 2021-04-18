5adb62ed1dbb38e72f035bed8934a637a582d2a8
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2021-04-19 01:42:45 +0530

    refactored variables

---
+++ modules/variables.js
@@ -0,0 +1,28 @@
+const roasts = [
+   " is as useless as the 'ueue' in the 'queue'",
+   ", mirrors can't talk. Lucky for you, they can't laugh either",
+   " is the reason the gene pool needs a lifeguard.",
+   ", if I had a face like yours, I'd sue my parents",
+   "'s only chance of getting laid is to crawl up a chicken's butt and wait.",
+   ", some day you'll go far... and I hope you stay there.",
+   " the Kyalané has visited us again",
+   " must have been born on a highway cos' that's where most accidents happen.",
+   ", if laughter is the best medicine, your face must be curing the world.",
+   " is your ass jealous of the amount of shit that just came out of your mouth?",
+   " I'd agree with you but then we'd both be wrong.",
+   ", if I had a dollar for everytime you said something smart, I'd be broke.",
+   ", when you were born, the doctor threw you out the window, but the window threw you back", 
+]
+
+const spartaImages = [
+   'https://scontent.fccu10-1.fna.fbcdn.net/v/t1.18169-9/27066982_1443381092450796_3057430696336939296_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=174925&_nc_ohc=KQ9wNU98H_EAX-4H79K&_nc_ht=scontent.fccu10-1.fna&oh=3764cab4e3e75cc47045e4f660bd9ce0&oe=60A0239E',
+   'https://scontent.fccu10-1.fna.fbcdn.net/v/t31.18172-8/1496023_455803201208595_2068773226_o.jpg?_nc_cat=110&ccb=1-3&_nc_sid=174925&_nc_ohc=dn9woPaug4QAX90t-yt&_nc_oc=AQmnjUBiB1UfgaRO5D0NFfXaxbv4EmfikAAaPIAQLtXS49vWgo5m-m7Y9W6zLSzG6rc&_nc_ht=scontent.fccu10-1.fna&oh=1eb70e521307df0c8aae988c464ac447&oe=60A205AA',
+   'https://scontent.fccu10-1.fna.fbcdn.net/v/t1.18169-9/11918961_761199650668947_3834636444262335503_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=19026a&_nc_ohc=3zi-RtGcij0AX8SigHm&_nc_ht=scontent.fccu10-1.fna&oh=ca9b02d42ae7c9d3bb19e16e936a617f&oe=60A26A61',
+   'https://scontent.fccu10-1.fna.fbcdn.net/v/t1.18169-9/12249982_791572684298310_7470982914109029885_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=174925&_nc_ohc=4LSG0wF9SscAX9uK98y&_nc_ht=scontent.fccu10-1.fna&oh=9c7f3d04a5423bace7229ea084287a2d&oe=60A008FD',
+   'https://scontent.fccu10-1.fna.fbcdn.net/v/t1.18169-9/10891643_645388568916723_572094079507358785_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=174925&_nc_ohc=W_0CRpbB2fIAX-1LVZ5&_nc_ht=scontent.fccu10-1.fna&oh=39e77d86a553b760f487422f54decc0c&oe=60A27C0E'
+]
+
+module.exports = {
+   roasts: roasts,
+   spartaImages: spartaImages
+};
